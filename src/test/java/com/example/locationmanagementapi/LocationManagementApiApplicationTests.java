e841f073a3514fb8b27dfdd4af0b190825a67171
mayank1409 <mayank_srivastava@yahoo.com>
2023-07-01 15:41:13 +0530

    initail commit

---
+++ src/test/java/com/example/locationmanagementapi/LocationManagementApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.locationmanagementapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LocationManagementApiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
